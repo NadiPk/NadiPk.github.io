95f69bc901a0f0c0b063028eb7d8c6934934d884
Pook <csav7091@uibk.ac.at>
2019-03-21 13:25:02 +0100

    4/7

--- learn java/main.js
+++ learn java/main.js
@@ -1,9 +1,12 @@
+//2
+
 public class HelloYou {
     public static void main(String[] args) {
         System.out.println("Hello Nadine!");
     }
 }
 
+//3
 public class Timeline {
     public static void main(String[] args) {
         System.out.println("Hello Java!");
@@ -21,3 +24,20 @@ public class Timeline {
         System.out.println("You are a fun language!");
     }
 }
+
+//4
+
+public class LanguageFacts {
+    public static void main(String[] args) {
+        // press enter or return on your keyboard after each semicolon!
+
+        System.out.println("Java is a class-based language.");
+        System.out.println("Java classes have a 'main' method.");
+        System.out.println("Java statements end with a semicolon.");
+
+        System.out.println("Programming is amaaaazing!");
+    }
+}
+
+//5
+
