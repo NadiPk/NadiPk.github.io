edd971f724a1b384d4d1add6d48331d5953d4dea
Pook <csav7091@uibk.ac.at>
2019-03-21 14:02:57 +0100

    8/9

--- learn java/main.js
+++ learn java/main.js
@@ -160,3 +160,15 @@ public class Mess {
         boolean isPG = true;
     }
 }
+
+//8
+
+public class BadNames {
+    public static void main(String[] args) {
+        String firstName = "Samira";
+        String lastName = "Smith";
+        String email = "samira@google.com";
+        int salaryExpectation = 100000;
+        int yearOfBirth = 1955;
+    }
+}
