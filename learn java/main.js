2b04e4a68e2776f06c96d0364c88ebfded1a691d
Pook <csav7091@uibk.ac.at>
2019-03-21 13:53:07 +0100

    3/9

--- learn java/main.js
+++ learn java/main.js
@@ -70,26 +70,26 @@ public class Compiling {
 //7
 
 public class Review {
-    public static void main(String[] args){
-      //The main method executes the tasks of the class
-      System.out.println("My first Java program from scratch!");
-      
+    public static void main(String[] args) {
+        //The main method executes the tasks of the class
+        System.out.println("My first Java program from scratch!");
+
     }
-    
-  }
+
+}
 
 
 
-  //1/9
+//1/9
 
-  public class Creator {
-	public static void main(String[] args) {
-    String name = "James Gosling";
-    int yearCreated = 1995;
-    
-    System.out.println(name);
-    System.out.println(yearCreated);
-	}
+public class Creator {
+    public static void main(String[] args) {
+        String name = "James Gosling";
+        int yearCreated = 1995;
+
+        System.out.println(name);
+        System.out.println(yearCreated);
+    }
 }
 
 
@@ -98,17 +98,24 @@ public class Review {
 //This is the class declaration:
 public class CountComment {
     //This is the main method that runs when you compile:
-      public static void main(String[] args) {
-      //This is where you will define your variable:
-      int numComments = 6;
-      //This is where you will print your variable:
-          System.out.println(numComments);
+    public static void main(String[] args) {
+        //This is where you will define your variable:
+        int numComments = 6;
+        //This is where you will print your variable:
+        System.out.println(numComments);
     }
-    
-  //This is the end of the class:
-  }
-  
-  //This is outside the class
 
-  //3
-  
+    //This is the end of the class:
+}
+
+//This is outside the class
+
+
+//3
+
+public class MarketShare {
+    public static void main(String[] args) {
+        double androidShare = 81.7;
+        System.out.println(androidShare);
+    }
+}
