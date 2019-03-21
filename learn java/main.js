20770b4d21a5b38da174d5d5f27f66938de18f45
Pook <csav7091@uibk.ac.at>
2019-03-21 14:00:38 +0100

    7/9

--- learn java/main.js
+++ learn java/main.js
@@ -148,3 +148,15 @@ public class Song {
 
     }
 }
+
+//7
+
+public class Mess {
+    public static void main(String[] args) {
+        int year = 2001;
+        String title = "Shrek";
+        char genre = 'C';
+        double runtime = 1.58;
+        boolean isPG = true;
+    }
+}
