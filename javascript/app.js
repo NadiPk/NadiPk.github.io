bd3392a267463b4a1b2f4989071bc8394164e60f
Pook <csav7091@uibk.ac.at>
2019-03-26 16:03:20 +0100

    9/10

--- javascript/app.js
+++ javascript/app.js
@@ -35,3 +35,8 @@ console.log('Teaching the world how to code'.length);
 //8
 console.log('Codecademy'.toUpperCase());
 console.log('    Remove whitespace   '.trim());
+
+//9
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
