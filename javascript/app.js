e1bc544c310df3a6ac8b437eebe033a8132729cf
Pook <csav7091@uibk.ac.at>
2019-03-26 15:54:03 +0100

    5/10

--- javascript/app.js
+++ javascript/app.js
@@ -20,3 +20,9 @@ console.log(2011);
 console.log('Woohoo! I love to code! #codecademy');
 console.log(20.49);
 
+//5
+console.log(29 + 3.5); // Prints 32.5
+console.log(2019 - 1969); // Prints 50
+console.log(65 / 240); // Prints 0,27083333333333
+console.log(0.2708 * 100); // Prints 27,08
+
