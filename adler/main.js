7e95e60d4b40086c42ac34720e5bcdabb4ced58d
Pook <csav7091@uibk.ac.at>
2019-03-27 16:00:07 +0100

    3

---
+++ adler/main.js
@@ -0,0 +1,29 @@
+//alert("Hallo Welt!");
+
+const div = document.getElementById ("map");
+const breite = div.getAttribute ("data-lat");
+const laenge = div.getAttribute ("data-lng");
+const titel = div.getAttribute ("data-title");
+
+//console.log("Breite=",breite,"Länge=",laenge,"Titel=",titel);
+
+//Karte initialisieren
+
+let karte = L.map("map");
+//console.log(karte);
+
+//auf Ausschnitt zoomen
+karte.setView(
+    [breite,laenge],
+    13
+);
+
+//openstreetmap einbauen
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
+
+let pin = L.marker (
+    [breite,laenge]
+).addTo(karte);
+
+//popup zum pin hängen
+pin.bindPopup(titel).openPopup();
