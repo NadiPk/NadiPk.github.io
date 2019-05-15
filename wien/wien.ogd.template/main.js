d40ec35b60b98664e727dad93fa330597320dc8a
Pook <csav7091@uibk.ac.at>
2019-05-15 18:00:32 +0200

    searchfenster

--- wien/wien.ogd.template/main.js
+++ wien/wien.ogd.template/main.js
@@ -69,4 +69,54 @@ karte.addControl(new L.Control.Fullscreen());
 
 karte.setView([48.208333, 16.373056], 12);
 
+const url = 'https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SPAZIERPUNKTOGD%20&srsName=EPSG:4326&outputFormat=json'
+
+function makeMarker(feature, latlng) {
+    const fotoIcon = L.icon({
+        iconUrl: 'http://www.data.wien.gv.at/icons/sehenswuerdigogd.svg',
+        iconSize: [36, 36]
+
+    });
+    const sightMarker = L.marker(latlng, {
+        icon: fotoIcon
+    })
+    sightMarker.bindPopup(`
+        <h3>${feature.properties.NAME}</h3>
+        <p>${feature.properties.BEMERKUNG}</p>
+        <hr>
+        <footer><a href=${feature.properties.WEITERE_INF}">Weblink</a></footer>
+    `);
+    return sightMarker;
+}
+
+async function loadSights(url) {
+    const clusterGruppe = L.markerClusterGroup();
+    const response = await fetch(url); //Fetch ABWARTEN
+    const sightsData = await response.json(); //umwandung in Jason u abwarten
+    const geoJson = L.geoJson(sightsData, {
+        pointToLayer: makeMarker
+
+    });
+    clusterGruppe.addLayer(geoJson);
+    karte.addLayer(clusterGruppe);
+    layerControl.addOverlay(clusterGruppe, "Sehenswürdigkeiten");
+const suchFeld = new L.Control.Search({
+layer: clusterGruppe,
+propertyName: "Name",
+zoom: 17
+});
+karte.addControl(suchFeld);
+
+}
+
+loadSights(url);
+
+const massstab = L.control.scale({
+    imperial: false,
+    metric: true
+});
+karte.addControl(massstab);
+
+
+
 // die Implementierung der Karte startet hier
