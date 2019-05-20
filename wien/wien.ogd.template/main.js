513734b2047f08a9f3cb827090f35f45712ac78b
NadiPk <https//:NadiPk/NadiPk.github.io.git>
2019-05-20 10:33:59 +0200

    repair

--- wien/wien.ogd.template/main.js
+++ wien/wien.ogd.template/main.js
@@ -1,5 +1,3 @@
-/* Wien OGD Beispiele */
-
 let karte = L.map("map");
 
 const kartenLayer = {
@@ -69,54 +67,130 @@ karte.addControl(new L.Control.Fullscreen());
 
 karte.setView([48.208333, 16.373056], 12);
 
-const url = 'https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SPAZIERPUNKTOGD%20&srsName=EPSG:4326&outputFormat=json'
+// die Implementierung der Karte startet hier
+
+const url = 'https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SPAZIERPUNKTOGD &srsName=EPSG:4326&outputFormat=json';
 
 function makeMarker(feature, latlng) {
     const fotoIcon = L.icon({
-        iconUrl: 'http://www.data.wien.gv.at/icons/sehenswuerdigogd.svg',
-        iconSize: [36, 36]
-
+        iconUrl: 'http://www.data.wien.gv.at/icons/sehenswuerdigogd.svg', //anderer Marker
+        iconSize: [16, 16]
     });
     const sightMarker = L.marker(latlng, {
         icon: fotoIcon
-    })
+    });
     sightMarker.bindPopup(`
         <h3>${feature.properties.NAME}</h3>
         <p>${feature.properties.BEMERKUNG}</p>
         <hr>
-        <footer><a href=${feature.properties.WEITERE_INF}">Weblink</a></footer>
-    `);
+        <footer><a target="blank" href="${feature.properties.WEITERE_INF}">Weblink</a></footer>
+        `); //Name, Beschreibung, Weblink (neuer Tab)
     return sightMarker;
 }
 
 async function loadSights(url) {
     const clusterGruppe = L.markerClusterGroup();
-    const response = await fetch(url); //Fetch ABWARTEN
-    const sightsData = await response.json(); //umwandung in Jason u abwarten
+    const response = await fetch(url);
+    const sightsData = await response.json();
     const geoJson = L.geoJson(sightsData, {
         pointToLayer: makeMarker
-
     });
+
+    //Clustergruppe
     clusterGruppe.addLayer(geoJson);
     karte.addLayer(clusterGruppe);
-    layerControl.addOverlay(clusterGruppe, "Sehenswürdigkeiten");
-const suchFeld = new L.Control.Search({
-layer: clusterGruppe,
-propertyName: "Name",
-zoom: 17
-});
-karte.addControl(suchFeld);
+    layerControl.addOverlay(clusterGruppe, "Sehenswürdigkeit");
 
+    //Suchfeld einfügen
+    const suchFeld = new L.Control.Search({
+        layer: clusterGruppe,
+        propertyName: "NAME",
+        zoom: 17,
+        initial: false,
+    });
+    karte.addControl(suchFeld);
 }
 
 loadSights(url);
 
-const massstab = L.control.scale({
+//Maßstab einfügen
+const scale = L.control.scale({
     imperial: false,
     metric: true
+
 });
-karte.addControl(massstab);
+karte.addControl(scale);
 
 
+//Spazierwege hinzufügen
+const wege = 'https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SPAZIERLINIEOGD &srsName=EPSG:4326&outputFormat=json';
 
-// die Implementierung der Karte startet hier
+function linienPopup(feature, layer) { //Wege Popup (nur Name möglich)
+    const popup = `
+        <h3>${feature.properties.NAME}</h3>
+        `;
+    layer.bindPopup(popup);
+}
+
+async function loadWege(wegeURL) {
+    const antwort = await fetch(wegeURL);
+    const wegeData = await antwort.json();
+    const wegeJson = L.geoJson(wegeData, {
+        style: function () { //Farbe der Wege
+            return {
+                color: "yellow"
+            };
+        },
+        onEachFeature: linienPopup
+    });
+
+    karte.addLayer(wegeJson);
+    layerControl.addOverlay(wegeJson, "Spazierwege");
+}
+loadWege(wege);
+
+
+//WLAN Standorte einfügen
+
+const wifi = 'https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:WLANWIENATOGD&srsName=EPSG:4326&outputFormat=json';
+
+function makeWifi(feature, latlng) {
+    const wifiIcon = L.icon({
+        iconUrl: 'http://www.data.wien.gv.at/icons/wlanwienatogd.svg', //anderer Marker
+        iconSize: [16, 16]
+    });
+    const wifiMarker = L.marker(latlng, {
+        icon: wifiIcon
+    });
+    wifiMarker.bindPopup(`
+        <h3>${feature.properties.NAME}</h3>
+        <p>${feature.properties.ADRESSE}</p>        
+        `); //Name, Beschreibung, ohne Weblink (neuer Tab)
+    return wifiMarker;
+}
+
+async function loadWifi(wifi) {
+    const clusterGruppewifi = L.markerClusterGroup();
+    const responsewifi = await fetch(wifi);
+    const wifiData = await responsewifi.json();
+    const geoJson = L.geoJson(wifiData, {
+        pointToLayer: makeWifi
+    });
+
+    //Clustergruppe
+    clusterGruppewifi.addLayer(geoJson);
+    karte.addLayer(clusterGruppewifi);
+    layerControl.addOverlay(clusterGruppewifi, "WLAN-Standorte");
+}
+
+//Suchfeld Wifi
+    // const suchFeldwifi = new L.Control.Search({
+    //     layer: clusterGruppewifi,
+    //     propertyName: "NAME",
+    //     zoom: 17,
+    //     initial: false,
+    // });
+    // karte.addControl(suchFeldwifi);
+
+
+loadWifi(wifi);
